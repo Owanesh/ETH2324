4ba2fcd3f38ab812e051f3f6611b48e8650eb832
Owanesh <24321561+Owanesh@users.noreply.github.com>
2024-04-29 15:24:42 +0200

    fix(tgc): now puppeteer has correct domain and host

--- tgc/src/app/api/26504013560767258174079178229644/route.tsx
+++ tgc/src/app/api/26504013560767258174079178229644/route.tsx
@@ -7,7 +7,7 @@ export async function POST(req: Request) {
     try {
 
         const body = await req.json();
-
+        const {headers} = req;
 
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
@@ -17,7 +17,9 @@ export async function POST(req: Request) {
         //add a timeout to the request
 
         try {
-            await page.goto("http://localhost:3000/YWRtaW4K/32721884973979946471658112221257", { waitUntil: 'networkidle0' });
+            const currentUrl = headers.host;
+            const path = "/YWRtaW4K/32721884973979946471658112221257"; // Replace this with your path
+            await page.goto(`http://${currentUrl}${path}`, { waitUntil: 'networkidle0' });
             await page.setCookie({
                 name: 'userData',
                 value: 'fc7fcd0dfff43f12ea176c886e9a6e98d2dc4645bc09f009bfcc78390947eb6c641dace3d9a2c6d9539b75ff2a9cb809f8a493e88a9a45b62ac52179a6b323c789c8fde69a7bea3c4ede6aeb71fa183f3ff0e5ef78d45f68c2b1dfcd2d0e7602f23c7990c61a5d1e6ac7da58383a1292d025411d159f89e28e64845f1c4bc64d5d9a0ee6dc44e267d0ae1e87d751506e8489c8ae420563bcf451fb52ebc99805',
@@ -27,12 +29,12 @@ export async function POST(req: Request) {
                 httpOnly: false,
                 secure: false
             });
-            await page.waitForSelector('button[type="submit"]'); // Attendi il caricamento del form
-            await page.type('#ftitle', body.title); // Compila il campo del titolo
-            await page.type('#fcontent', body.content); // Compila il campo del contenuto
-            await page.type('#ftags', body.tags); // Compila il campo dei tag
+            await page.waitForSelector('button[type="submit"]'); 
+            await page.type('#ftitle', body.title); 
+            await page.type('#fcontent', body.content); 
+            await page.type('#ftags', body.tags); 
             await Promise.all([
-                page.click('button[type="submit"]') // Fai clic sul pulsante di invio del form
+                page.click('button[type="submit"]') 
             ]);
             await page.waitForNavigation({ waitUntil: 'networkidle0' });
             await browser.close();
